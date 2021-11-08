42ea80b5e106c95469e3b968f6bffc5be8c5f14d
NokiOvO <97600123@qq.com>
2021-11-08 09:05:35 +0800

    commit

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/LostMemories/LostMemoriesGameModeBase.generated.h
@@ -0,0 +1,142 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LOSTMEMORIES_LostMemoriesGameModeBase_generated_h
+#error "LostMemoriesGameModeBase.generated.h already included, missing '#pragma once' in LostMemoriesGameModeBase.h"
+#endif
+#define LOSTMEMORIES_LostMemoriesGameModeBase_generated_h
+
+<<<<<<< HEAD
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_SPARSE_DATA
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+=======
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_SPARSE_DATA
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+>>>>>>> a4e4bb8efbaa354d3f69307f765b83379fed2192
+private: \
+	static void StaticRegisterNativesALostMemoriesGameModeBase(); \
+	friend struct Z_Construct_UClass_ALostMemoriesGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALostMemoriesGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LostMemories"), NO_API) \
+	DECLARE_SERIALIZER(ALostMemoriesGameModeBase)
+
+
+<<<<<<< HEAD
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS \
+=======
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS \
+>>>>>>> a4e4bb8efbaa354d3f69307f765b83379fed2192
+private: \
+	static void StaticRegisterNativesALostMemoriesGameModeBase(); \
+	friend struct Z_Construct_UClass_ALostMemoriesGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALostMemoriesGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LostMemories"), NO_API) \
+	DECLARE_SERIALIZER(ALostMemoriesGameModeBase)
+
+
+<<<<<<< HEAD
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+=======
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+>>>>>>> a4e4bb8efbaa354d3f69307f765b83379fed2192
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALostMemoriesGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALostMemoriesGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALostMemoriesGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALostMemoriesGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALostMemoriesGameModeBase(ALostMemoriesGameModeBase&&); \
+	NO_API ALostMemoriesGameModeBase(const ALostMemoriesGameModeBase&); \
+public:
+
+
+<<<<<<< HEAD
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+=======
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+>>>>>>> a4e4bb8efbaa354d3f69307f765b83379fed2192
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALostMemoriesGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALostMemoriesGameModeBase(ALostMemoriesGameModeBase&&); \
+	NO_API ALostMemoriesGameModeBase(const ALostMemoriesGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALostMemoriesGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALostMemoriesGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALostMemoriesGameModeBase)
+
+
+<<<<<<< HEAD
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_12_PROLOG
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_SPARSE_DATA \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+=======
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_12_PROLOG
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_SPARSE_DATA \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+>>>>>>> a4e4bb8efbaa354d3f69307f765b83379fed2192
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+<<<<<<< HEAD
+#define LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_SPARSE_DATA \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+=======
+#define LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_SPARSE_DATA \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+>>>>>>> a4e4bb8efbaa354d3f69307f765b83379fed2192
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LOSTMEMORIES_API UClass* StaticClass<class ALostMemoriesGameModeBase>();
+
+#undef CURRENT_FILE_ID
+<<<<<<< HEAD
+#define CURRENT_FILE_ID LostMemory_Source_LostMemories_LostMemoriesGameModeBase_h
+=======
+#define CURRENT_FILE_ID LostMemories8_Source_LostMemories_LostMemoriesGameModeBase_h
+>>>>>>> a4e4bb8efbaa354d3f69307f765b83379fed2192
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
